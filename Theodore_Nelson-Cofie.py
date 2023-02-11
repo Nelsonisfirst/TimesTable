8294163503e02df1b42094176a80e1867fa90db8
Nelsonisfirst <124259749+Nelsonisfirst@users.noreply.github.com>
2023-02-11 15:58:51 +0000

    Add files via upload

---
+++ Theodore_Nelson-Cofie.py
@@ -0,0 +1,31 @@
+#print("GETTY")
+
+#def multiplication():
+# x = 1
+# j = 1
+# for i in range(0,12):
+#     x = x+1
+#     for j in range(0,13):
+#         print(str(j) + " x " + str(j) + "=" + str(i * j))
+#         if j == 12:
+#             j = j +1
+
+
+# Multiplication table (from 1 to 12) in Python
+# To take input from the user
+num = int(input("Input a number times table you want to see: "))
+# Iterate 12 times from i = 1 to 12
+for i in range(0, 13):
+   print(f"{num} x {i} = {num*i}")
+
+
+
+
+
+
+
+
+
+
+
+
